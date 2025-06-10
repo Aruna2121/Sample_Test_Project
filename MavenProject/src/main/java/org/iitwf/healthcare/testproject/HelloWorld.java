06f2392a7da73238b7f843d08cad52c6a4ec03b7
13154 <13154@DESKTOP-IA8A2H9>
2025-06-10 11:04:36 -0400

    Commiting the code

---
+++ MavenProject/src/main/java/org/iitwf/healthcare/testproject/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.iitwf.healthcare.testproject;
+
+public class HelloWorld {
+	
+	public static void main(String[]args)
+	
+	{
+	System.out.println("Hello World!!!");
+	
+	System.out.println("Hello Aruna: This is your first program!!");
+	
+	}	
+
+}
+
+
